75652ce232a963b2f9f59e672aa46d5f3b0429ae
lionel_233 <lionel_233@95bef051-5249-4c04-9fec-13b8055f70d2>
2015-06-02 03:30:12 +0000

    teamData加入search功能，数据库地址改为localhost，

--- NBA_analysis3/NBA_Server/NBA_Server/src/main/java/BLservice/teamBLservice.java
+++ NBA_analysis3/NBA_Server/NBA_Server/src/main/java/BLservice/teamBLservice.java
@@ -1,5 +1,22 @@
 package BLservice;
 
-public class teamBLservice {
+import java.util.ArrayList;
 
+import po.TeamListItem;
+import data.teams.TeamNameListData;
+
+public class teamBLservice {
+    //for debug only
+    public static void main(String[] args){
+        TeamListItem item = new TeamListItem();
+        item.setConference("eastern");
+        item.setTeamNameEn("ATL");
+        
+        TeamNameListData data = new TeamNameListData();
+        data.init();
+        ArrayList<TeamListItem> list = data.getTeamList(item);
+        data.finish();
+        
+        System.out.println(list.size() );
+    }
 }
